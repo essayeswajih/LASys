1012d347166e90677aeda6abcdccc087a78af2db
essayeswajih <wajihsayes@gmail.com>
2024-10-30 18:01:58 +0100

    rowDTOChanged

--- schemas/logDTO.py
+++ schemas/logDTO.py
@@ -2,17 +2,11 @@ from typing import List, Optional
 from pydantic import BaseModel
 from models.rowEntity import Row
 class LogDTO(BaseModel):
-    id : Optional[int] = None
-    rows : Optional[List[Row]] = []
-    log_of : Optional[str] = None
-    file_name : Optional[str] = None
-    file_type : Optional[str] = None
-    protocol : Optional[str] = None
-    src_port : Optional[int] = None
-    dest_port : Optional[int] = None
-    message : Optional[str] = None
-    level : Optional[str] = None
-    component : Optional[str] = None
+    id: Optional[int] = None
+    rows: Optional[List[Row]] = []
+    log_of: Optional[str] = None
+    file_name: Optional[str] = None
+    file_type: Optional[str] = None
     class Config:
         from_attributes = True
         arbitrary_types_allowed = True
@@ -21,12 +15,6 @@ class LogCreate(BaseModel):
     log_of: Optional[str] = None
     file_name: Optional[str] = None
     file_type: Optional[str] = None
-    protocol : Optional[str] = None
-    src_port : Optional[int] = None
-    dest_port : Optional[int] = None
-    message : Optional[str] = None
-    level : Optional[str] = None
-    component : Optional[str] = None
 
     class Config:
         from_attributes = True  # Allow reading data from ORMs
